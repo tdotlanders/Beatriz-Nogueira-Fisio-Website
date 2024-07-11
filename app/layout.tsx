45a40498dcbde07774a42bae1e8fb12fbe918af1
Marco Ventura <venturapm2000@gmail.com>
2024-07-11 19:32:37 +0100

    made header and ajusted css classes to match the theme
    also changed the favicon file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,11 @@
+import Header from "@/components/header";
 import "./globals.css";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Marco | Personal Portfolio",
+  title: "Beatriz Nogueira | Fisioterapeuta",
   description: "Marco is a Full Stack Developer eager to connect to the world!",
 };
 
@@ -15,9 +16,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-gray-50 text-gray-950 relative`}>
-        <div className="bg-[#FFD7EF] absolute top- [-6rem] -z-10  right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] "></div>
-        <div className="bg-[#C7FFD3] absolute top- [-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[60rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
+      <body
+        className={`${inter.className} bg-gray-50 text-gray-950 relative h-[5000px]`}
+      >
+        <div className="bg-[#85dbf5] absolute top- [-6rem] -z-10  right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] "></div>
+        <div className="bg-[#E4BBFF] absolute top- [-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[60rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
+        <Header />
         {children}
       </body>
     </html>
